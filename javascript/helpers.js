4e685bdb8469b606365b00d8e4e2cd141faf917a
Jonas Baumann <jone@jone.ch>
2013-06-09 11:50:16 +0200

    Implement compare_requirement_list.

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -22,6 +22,28 @@ function parse_version(version) {
 }
 
 
+function compare_requirement_list(version, requirements) {
+  version = parse_version(version);
+
+  for (var r=0; r<requirements.length; r++) {
+    var first = parse_version(requirements[r][0]);
+    var last = requirements[r][1] !== null ? parse_version(requirements[r][1]) : null;
+
+    if (last === null && version >= first) {
+      return true;
+    }
+
+    if (version >= first && version <= last) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+
 if(typeof(module) !== "undefined") {
+  module.exports.parse_version = parse_version;
   module.exports.compare_requirement_list = compare_requirement_list;
 }
--- test/test_helpers.js
+++ test/test_helpers.js
@@ -17,3 +17,39 @@ describe('parse_version', function() {
   });
 
 });
+
+
+describe('compare_requirement_list', function() {
+  var compare_requirement_list = require('../javascript/helpers.js').compare_requirement_list;
+
+  it('should return true when the version matches', function() {
+    assert.equal(true, compare_requirement_list("2.3", [["2.1", "2.4"]]));
+  });
+
+  it('should return false when the version matches', function() {
+    assert.equal(false, compare_requirement_list("2.7", [["2.1", "2.4"]]));
+  });
+
+  it('should include the first version of a requirement (>=)', function() {
+    assert.equal(true, compare_requirement_list("2.1", [["2.1", "2.4"]]));
+  });
+
+  it('should include the last version of a requirement (<=)', function() {
+    assert.equal(true, compare_requirement_list("2.4", [["2.1", "2.4"]]));
+  });
+
+  it('should return true when last version is null (not yet fixed in stable version)', function() {
+    assert.equal(true, compare_requirement_list("3.7", [["2.1", null]]));
+  });
+
+  it('should return true when at least one requirement matches', function() {
+    assert.equal(true, compare_requirement_list("2.3",
+                                                 [["3.7", "3.8"], ["2.1", "2.4"]]));
+  });
+
+  it('should return false when no requirement matches', function() {
+    assert.equal(false, compare_requirement_list("4",
+                                                 [["3.7", "3.8"], ["2.1", "2.4"]]));
+  });
+
+});
