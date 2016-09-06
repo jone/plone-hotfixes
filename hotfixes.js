45034aa6047dc76a1e60c9a7c0e9495debff1bd7
Jonas Baumann <jone@jone.ch>
2016-09-06 09:21:12 +0200

    update plone.org urls.

--- hotfixes.js
+++ hotfixes.js
@@ -18,14 +18,14 @@ var hotfixes = {
           ["5.1a1", "5.1a1"]
     ],
     "fixed_in_plone": ["4.3.10", "5.0.5"],
-    "plone.org": "https://plone.org/products/plone/security/advisories/20160419-preannounce"},
+    "plone.org": "https://plone.org/security/hotfix/20160419"},
 
   "plone4.csrffixes": {
     "required_for_plone": [
         ["4", "4.99.??"]
     ],
     "fixed_in_plone": ["5.0a1"],
-    "plone.org": "https://plone.org/products/plone-hotfix/releases/20151006"},
+    "plone.org": "https://plone.org/security/hotfix/20151006"},
 
   "Products.PloneHotfix20151208": {
     "required_for_plone": [
@@ -33,7 +33,7 @@ var hotfixes = {
         ["5.0a1", "5.0"]
     ],
     "fixed_in_plone": ["4.3.8", "5.0.1"],
-    "plone.org": "https://plone.org/products/plone-hotfix/releases/20151208"},
+    "plone.org": "https://plone.org/security/hotfix/20151208"},
 
   "Products.PloneHotfix20150910": {
     "required_for_plone": [
@@ -41,14 +41,14 @@ var hotfixes = {
         ["5.0a1", "5.0rc1"]
     ],
     "fixed_in_plone": ["4.3.7", "5.0rc2"],
-    "plone.org": "https://plone.org/products/plone-hotfix/releases/20150910"},
+    "plone.org": "https://plone.org/security/hotfix/20150910"},
 
   "Products.PloneHotfix20131210": {
     "required_for_plone": [
         ["3.3", "4.3.2"]
     ],
     "fixed_in_plone": ["4.3.3"],
-    "plone.org": "https://plone.org/security/20131210"},
+    "plone.org": "https://plone.org/security/hotfix/20131210"},
 
   "Products.PloneHotfix20130618": {
     "required_for_plone": [
@@ -56,7 +56,7 @@ var hotfixes = {
         ["4.3a1", "4.3.1"]
     ],
     "fixed_in_plone": ["4.2.6", "4.3.2"],
-    "plone.org": "http://plone.org/products/plone/security/advisories/20130611-announcement"},
+    "plone.org": "https://plone.org/security/hotfix/20130618"},
 
   "Products.PloneHotfix20121106": {
     "required_for_plone": [
@@ -64,7 +64,7 @@ var hotfixes = {
         ["4.3a1", "4.3a2"]
     ],
     "fixed_in_plone": ["4.2.3", "4.3b1"],
-    "plone.org": "http://plone.org/products/plone-hotfix/releases/20121106"},
+    "plone.org": "https://plone.org/security/hotfix/20121106"},
 
   "Products.Zope_Hotfix_20111024": {
     "required_for_plone": [
@@ -81,14 +81,14 @@ var hotfixes = {
         ["4.2a1", "4.2a2"]
     ],
     "fixed_in_plone": ["4.0.10", "4.1.1", "4.2a3"],
-    "plone.org": "http://plone.org/products/plone-hotfix/releases/20110928"},
+    "plone.org": "https://plone.org/security/hotfix/20110928"},
 
   "Products.PloneHotfix20110720": {
     "required_for_plone": [
         ["2.0", "4.0.3"]
     ],
     "fixed_in_plone": ["4.0.4"],
-    "plone.org": "http://plone.org/products/plone-hotfix/releases/CVE-2011-0720"},
+    "plone.org": "https://plone.org/security/hotfix/20110720"},
 
   "Products.Zope_Hotfix_20110622": {
     "required_for_plone": [
@@ -97,13 +97,13 @@ var hotfixes = {
         ["4.1a1", "4.1rc3"]
     ],
     "fixed_in_plone": ["3.3.6", "4.0.8", "4.1"],
-    "plone.org": "http://plone.org/products/plone-hotfix/releases/20110622"},
+    "plone.org": "https://plone.org/security/hotfix/20110622"},
 
   "Products.PloneHotfix20110531": {
     "required_for_plone": [
         ["3.0", "4.0.6"]],
     "fixed_in_plone": ["4.0.7", "4.1rc3"],
-    "plone.org": "http://plone.org/products/plone-hotfix/releases/20110531"},
+    "plone.org": "https://plone.org/security/hotfix/20110531"},
 
 
   "Products.Zope_Hotfix_CVE_2010_3198": {
